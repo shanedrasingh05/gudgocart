6b0c2c2f1933f8ca806d1d63d6dfa2b10a5cd0e2
Shanedra Singh <shanedrasingh89@gmail.com>
2025-04-05 03:51:42 +0530

    add file

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+
+  const value = { navigate, user, setUser, isSeller, setIsSeller };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+// import { createContext } from "react";
+// import { useNavigate} from "react-router-dom";
+
+// export const AppContext = createContext();
+
+// export const AppContextProvider = ({ children }) => {
+
+//     const navigate = useNavigate();
+//     const [user, setUser] = useState(null);
+//     const [isSeller, setIsSeller] = useState(false);
+
+//     const value = {navigate, user, setUser, isSeller, setIsSeller  }  // provide state and methods to the rest of the app};
+
+//     return (
+//         <AppContext.Provider value={value}>
+//             {children}
+//         </AppContext.Provider>
+//     );
+// };
+
+// export const useAppContext = () => {
+//     return React.useContext(AppContext);
+// }
