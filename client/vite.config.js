cd688422c951e4e5f8fe511d9085ea781aede971
Shanedra Singh <shanedrasingh89@gmail.com>
2025-04-25 15:39:18 +0530

    add file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
+  base: "/", // 👈 Add this line!
+  plugins: [react(), tailwindcss()],
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//     tailwindcss(),
+//   ],
+// })
